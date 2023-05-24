5fd9861a87fd411168f8c0d364815cf884820fa3
opnxj <opnxj@protonmail.com>
2023-05-24 15:13:36 +0800

    Feat: Make contract abstract

--- src/SuperwhitelistERC20.sol
+++ src/SuperwhitelistERC20.sol
@@ -4,56 +4,67 @@ pragma solidity ^0.8.13;
 import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract SuperwhitelistERC20 is ERC20, Ownable {
+/**
+ * @title SuperwhitelistERC20
+ * @author opnxj
+ * @dev This contract extends Solmate's ERC20 ipmlementation and adds blacklist/superwhitelist functionality.
+ *      Users can be added to the superwhitelist by the contract owner, which gives immunity from being blacklisted.
+ *      The contract owner can add and remove addresses from the blacklist.
+ *      Transfers involving a blacklisted address will be reverted.
+ */
+abstract contract SuperwhitelistERC20 is ERC20, Ownable {
     mapping(address => bool) public superwhitelist;
     mapping(address => bool) public blacklist;
 
-    constructor() ERC20("SuperwhitelistERC20", "SWL", 18) {
-        // Mint 1000 tokens to the deployer, for testing
-        _mint(msg.sender, 1000 ether);
-    }
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
+
+    event AddedToSuperwhitelist(address indexed _addr);
+    event AddedToBlacklist(address indexed _addr);
+    event RemovedFromBlacklist(address indexed _addr);
 
     error Error_SuperwhitelistedCannotBeBlacklisted();
     error Error_BlacklistedCannotTransfer();
 
-    modifier notSuperwhitelisted(address _addr) {
+    function addToSuperwhitelist(address _addr) public virtual onlyOwner {
+        superwhitelist[_addr] = true;
+        emit AddedToSuperwhitelist(_addr);
+    }
+
+    function addToBlacklist(address _addr) public virtual onlyOwner {
         if (superwhitelist[_addr]) {
             revert Error_SuperwhitelistedCannotBeBlacklisted();
         }
-        _;
-    }
-
-    modifier notBlacklisted(address _addr) {
-        if (blacklist[_addr]) {
-            revert Error_BlacklistedCannotTransfer();
-        }
-        _;
-    }
-
-    function addToSuperwhitelist(address _addr) external onlyOwner {
-        superwhitelist[_addr] = true;
-    }
-
-    function addToBlacklist(
-        address _addr
-    ) external onlyOwner notSuperwhitelisted(_addr) {
         blacklist[_addr] = true;
+        emit AddedToBlacklist(_addr);
     }
 
-    function removeFromBlacklist(address _addr) external onlyOwner {
+    function removeFromBlacklist(address _addr) public virtual onlyOwner {
         blacklist[_addr] = false;
+        emit RemovedFromBlacklist(_addr);
     }
 
     function transfer(
         address _to,
         uint256 _amount
-    )
-        public
-        override
-        notBlacklisted(msg.sender)
-        notBlacklisted(_to)
-        returns (bool)
-    {
-        super.transfer(_to, _amount);
+    ) public virtual override returns (bool) {
+        if (blacklist[msg.sender] || blacklist[_to]) {
+            revert Error_BlacklistedCannotTransfer();
+        }
+        return super.transfer(_to, _amount);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public virtual override returns (bool) {
+        if (blacklist[_from] || blacklist[_to]) {
+            revert Error_BlacklistedCannotTransfer();
+        }
+        return super.transferFrom(_from, _to, _amount);
     }
 }
--- test/SuperwhitelistERC20.t.sol
+++ test/SuperwhitelistERC20.t.sol
@@ -1,36 +1,66 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test} from "../lib/forge-std/src/Test.sol";
 import {SuperwhitelistERC20} from "../src/SuperwhitelistERC20.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
+
+contract SuperwhitelistToken is SuperwhitelistERC20 {
+    constructor() SuperwhitelistERC20("Superwhitelist Token", "SWLT", 18) {}
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+}
 
 contract SuperwhitelistERC20Test is Test {
-    SuperwhitelistERC20 public swl;
+    SuperwhitelistToken public swl;
+
+    address public alice = address(0x123); // To superwhitelist
+    address public bob = address(0x456); // To blacklist
+    address public charlie = address(0x789);
 
     function setUp() public {
-        swl = new SuperwhitelistERC20();
-    }
-
-    function testBlacklist() public {
-        address bad = address(0x1);
-        swl.addToBlacklist(bad);
-        vm.expectRevert(
-            SuperwhitelistERC20.Error_BlacklistedCannotTransfer.selector
-        );
-        swl.transfer(bad, 1); // Try transfer, show that it fails
-
-        address good = address(0x2);
-        swl.transfer(good, 1); // Show that this works
+        swl = new SuperwhitelistToken();
+        swl.mint(alice, 1);
+        swl.mint(bob, 1);
+        swl.mint(charlie, 1);
     }
 
     function testSuperwhitelist() public {
-        address good = address(0x2);
-        swl.addToSuperwhitelist(good);
+        swl.addToSuperwhitelist(alice);
+        // Cannot blacklist after superwhitelisting
         vm.expectRevert(
             SuperwhitelistERC20
                 .Error_SuperwhitelistedCannotBeBlacklisted
                 .selector
         );
-        swl.addToBlacklist(good); // Try blacklist, show that it fails
+        swl.addToBlacklist(alice);
+    }
+
+    function testBlacklist() public {
+        swl.addToBlacklist(bob);
+
+        // Bob cannot transfer
+        vm.prank(bob);
+        vm.expectRevert(
+            SuperwhitelistERC20.Error_BlacklistedCannotTransfer.selector
+        );
+        swl.transfer(charlie, 1);
+
+        // Cannot transfer to Bob
+        vm.prank(alice);
+        vm.expectRevert(
+            SuperwhitelistERC20.Error_BlacklistedCannotTransfer.selector
+        );
+        swl.transfer(bob, 1);
+
+        // Alice cannot transferFrom Bob after approval
+        vm.prank(bob);
+        swl.approve(alice, 1);
+        vm.prank(alice);
+        vm.expectRevert(
+            SuperwhitelistERC20.Error_BlacklistedCannotTransfer.selector
+        );
+        swl.transferFrom(bob, alice, 1);
     }
 }
